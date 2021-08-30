c7c623678ce69fb6bc650ea0dd0ce32624a2f7a9
Sunny <sunhee5839@naver.com>
2021-08-30 17:56:57 +0900

    offering upload

--- js/index.js
+++ js/index.js
@@ -5,4 +5,16 @@ $(function () {
   $(".pop-schedule .btn_more-agree").click(function () {
     $(".pop-schedule").hide();
   });
+
+  $(".visual-content").hide();
+  $(".tabFirst").show();
+
+  $(".visual-menu a").click(function (e) {
+    e.preventDefault();
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    var idx = $(".visual-menu a").index(this);
+    $(".visual-content").hide();
+    $(".visual-content").eq(idx).show();
+  });
 });
