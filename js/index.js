f3222937d0d83a47b591495589e49697cfbb5ca9
Sunny <sunhee5839@naver.com>
2021-08-30 15:53:42 +0900

    img liveserver upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".offering-info .offering-schedule").click(function () {
+    $(".pop-schedule").show();
+  });
+  $(".pop-schedule .btn_more-agree").click(function () {
+    $(".pop-schedule").hide();
+  });
+});
